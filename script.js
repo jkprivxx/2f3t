d79542f8def9d5055afaa7d464f843b586db9ab0
afonso cavichioli <108934804+jkprivxx@users.noreply.github.com>
2023-09-18 08:09:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 mensagem = (msg) => alert(msg);
 soma = (a,b) => a + b;
-
-
+sub = (a,b) => a - b;
+mult = (a,b) => a * b;
+div = (a,b) => a / b;
+raiz = x => Math.sqrt(x);
+// mensagem (soma(10, soma(0,2)));
+//mensagem (soma(2, div(2,2)));

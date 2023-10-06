1338c1eb7f95771161ff0739e4151d371ac4f64c
afonso cavichioli <108934804+jkprivxx@users.noreply.github.com>
2023-10-05 21:43:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
-mensagem = (msg) => alert(msg);
-soma = (a,b) => a + b;
-sub = (a,b) => a - b;
-mult = (a,b) => a * b;
-div = (a,b) => a / b;
+mensagem = (msg) => alert (msg);
+
+soma = (a,b) => a+b;
+sub = (a,b) => a-b;
+mult = (a,b) => a*b;
+div = (a,b) => a/b;
 raiz = x => Math.sqrt(x);
-// mensagem (soma(10, soma(0,2)));
-//mensagem (soma(2, div(2,2)));
+let a ='';
+let b ='';
+let valor ='';
+let temPonto = false;
+//mensagem(soma (10, soma(8+2)));
+//mensagem (soma (2, div(2,2)));
+//mensagem (soma (div(80,2) ,mult(5,3)));
+//mensagem (soma (sub(8,5), sub(3,70)));
+
+function escrever_display(num){
+
+document.getElementById("resultado").value = num;
+
+}
+
+function digitando (tecla){
+
+if (tecla == "."){
+    if(!temPonto){
+        temPonto = true;
+        valor += tecla;
+        escrever_diplay(valor);
+    }
+    return
+}    
+valor += tecla;
+escrever_display(valor)
+
+}
+
+let executa = "";
+function  operacao (op){
+    executa = op;
+    a = valor;
+    valor = "";
+    temPonto = false;
+}
+
+function calcula(){
+    if (executa != ""){
+        b = valor;
+        if (executa == "mult") escrever_display(mult(a,b));
+        if (executa == "div") escrever_display(div(a,b));
+        if (executa == "sub") escrever_display(sub(a,b));
+        if (executa == "soma") escrever_display(soma(Number(a),Number(b)));
+        a = "";
+        b = "";
+        valor = "";
+        temPonto = false;
+    }
+}
